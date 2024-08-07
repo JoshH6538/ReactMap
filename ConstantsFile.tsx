de4ff8f1a62f8768bfedbcdff95e6a20247c437b
JoshH6538 <143372242+JoshH6538@users.noreply.github.com>
2024-08-06 17:06:23 -0700

    added login / logout functionality w/ token saved to localStorage. Just imported axios

---
+++ ConstantsFile.tsx
@@ -0,0 +1,18 @@
+
+const Constants = {
+    // DEV ID
+    CLIENT_ID: "61b76b12c48648a8aaf3b1a270b8c7f5",
+    // Authorization page
+    SPOTIFY_AUTHORIZE_ENDPOINT: "https://accounts.spotify.com/authorize",
+    //redirect back to site
+    REDIRECT_URL_AFTER_LOGIN: "http://localhost:5173/",
+    //permissions from api at https://developer.spotify.com/documentation/web-api/concepts/scopes
+    REACT_APP_BASE_URL: "http://localhost:5173",
+    CLIENT_SECRET: "1034d5e458324454bd304f61a711a20f",
+    SCOPES: ["user-top-read"],
+    SPACE_DELIM: "%20",
+}
+
+
+
+export default Constants;
